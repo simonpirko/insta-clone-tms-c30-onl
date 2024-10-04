f931610a9aa9d04a678dcd8b0eaa747dc1a8b5d4
alex garag <alex.garag@bk.ru>
2024-10-04 09:04:38 +0300

    tms-57 Ego sumam eam

--- src/main/java/by/tms/instaclone/servlet/RegistrationServlet.java
+++ src/main/java/by/tms/instaclone/servlet/RegistrationServlet.java
@@ -1,10 +1,10 @@
+
 package by.tms.instaclone.servlet;
 
 import by.tms.instaclone.storage.UsernamesStorage;
 import by.tms.instaclone.storage.UsersStorage;
 
 import by.tms.instaclone.utilites.ValidateData;
-import by.tms.instaclone.model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -31,37 +31,24 @@ public class RegistrationServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        //создать юзера, если он уникален и сохранить.
-        UsersStorage usersStorage = UsersStorage.getInstance();
-        ConcurrentHashMap<String, UUID> usernames = UsernamesStorage.getInstance().getUsernames();
-        if (usernames.get(username) == null) {
-            usersStorage.newUser(name, username, password);
-        } else {
-            System.out.println("Username already exists");
-        }
 
-//        resp.sendRedirect("/");
-//        req.setAttribute("name", name);
-//        req.setAttribute("username", username);
-//        req.setAttribute("password", password);
-//        if (ValidateData.validateName(name) && ValidateData.validateUsername(username) && ValidateData.validatePassword(password)) {
-//            usersStorage.getUsernames();
-//            if(usernames.get(username)==null){
-//                User user=new User(name,username,password);
-//                usersStorage.newUser(user);
-//                resp.sendRedirect("/login");
-//            } else {
-//                req.setAttribute("message","Username already exists");
-//                req.getRequestDispatcher("/pages/reg.jsp").forward(req,resp);
-//            }
-//        } else {
-//            String errorName = ValidateData.getErrorValidateName(name);
-//            String errorUsername = ValidateData.getErrorValidateUsername(username);
-//            String errorPassword = ValidateData.getErrorValidatePassword(password);
-//            req.setAttribute("errorName", errorName);
-//            req.setAttribute("errorUsername", errorUsername);
-//            req.setAttribute("errorPassword", errorPassword);
-//            req.getRequestDispatcher("/pages/reg.jsp").forward(req, resp);
-//        }
+        if (ValidateData.validateName(name) && ValidateData.validateUsername(username) && ValidateData.validatePassword(password)) {
+            ConcurrentHashMap<String, UUID> usernames = UsernamesStorage.getInstance().getUsernames();
+            if(usernames.get(username)==null){
+                usersStorage.newUser(name,username,password);
+                resp.sendRedirect("/login");
+            } else {
+                req.setAttribute("message","Username already exists");
+                req.getRequestDispatcher("/pages/reg.jsp").forward(req,resp);
+            }
+        } else {
+            String errorName = ValidateData.getErrorValidateName(name);
+            String errorUsername = ValidateData.getErrorValidateUsername(username);
+            String errorPassword = ValidateData.getErrorValidatePassword(password);
+            req.setAttribute("errorName", errorName);
+            req.setAttribute("errorUsername", errorUsername);
+            req.setAttribute("errorPassword", errorPassword);
+            req.getRequestDispatcher("/pages/reg.jsp").forward(req, resp);
+        }
     }
 }
