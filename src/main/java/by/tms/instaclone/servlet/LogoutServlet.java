213b0e58dd812c58ee8d77efae759fa5a87bec6a
Daniil <daniillalala14@gmail.com>
2024-10-04 21:36:55 +0300

    Create a LogoutServlet

---
+++ src/main/java/by/tms/instaclone/servlet/LogoutServlet.java
@@ -0,0 +1,20 @@
+package by.tms.instaclone.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static by.tms.instaclone.storage.KeeperConstants.LOGIN_PATH;
+import static by.tms.instaclone.storage.KeeperConstants.LOGOUT_PATH;
+
+@WebServlet(LOGOUT_PATH)
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect(LOGIN_PATH);
+    }
+}
