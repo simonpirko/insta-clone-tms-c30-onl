e05ed4cb3235207094f572edffd083b3a14a3535
Raman <rdubouski@gmail.com>
2024-10-09 18:13:41 +0300

    NewPostServlet complete

--- src/main/java/by/tms/instaclone/servlet/NewPostServlet.java
+++ src/main/java/by/tms/instaclone/servlet/NewPostServlet.java
@@ -1,6 +1,8 @@
 package by.tms.instaclone.servlet;
 
+import by.tms.instaclone.model.Post;
 import by.tms.instaclone.model.User;
+import by.tms.instaclone.storage.PhotoStorage;
 import by.tms.instaclone.storage.PostsStorage;
 
 import javax.servlet.ServletException;
@@ -21,6 +23,7 @@ import static by.tms.instaclone.storage.KeeperConstants.*;
 public class NewPostServlet extends HttpServlet {
 
     PostsStorage postsStorage = PostsStorage.getInstance();
+    PhotoStorage photoStorage = PhotoStorage.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,26 +34,21 @@ public class NewPostServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User curUser = (User) req.getSession().getAttribute(CURRENT_USER_ATTRIBUTE);
         Collection<Part> parts = req.getParts();
+        String textPost = req.getParameter("textPost");
         int count = 0;
         for (Part part : parts) {
             count++;
-            if (count > 6) {
+            if (count >= 6) {
                 req.setAttribute("errorMax", "Maximum 5 photos per post");
                 req.getRequestDispatcher(NEW_POST_JSP).forward(req, resp);
             }
         }
+        Post post = postsStorage.newPost(curUser, textPost);
         for (Part part : parts) {
             if (part.getName().equals("photosMultiple")) {
-
-            }
-            if (part.getName().equals("textPost")) {
-
+                photoStorage.addPhoto(post, part);
             }
         }
-
-//        byte[] avatarBytes = avatar.getInputStream().readAllBytes();
-
-
         resp.sendRedirect("/");
     }
 }
